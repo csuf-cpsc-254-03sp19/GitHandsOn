8ddec48948e81e61b25d29feacb623e8f884c895
ktruong52 <ktruong0424@gmail.com>
2019-02-28 20:50:15 -0800

    Adding the board, moves and winner parameters

--- tictactoe.py
+++ tictactoe.py
@@ -4,7 +4,12 @@ import random
 import sys
 
 #define board 
-board=[]
+board=[i for i in range(0,9)]
+player, computer = '',''
+# Corners, Center and Others, respectively
+moves=((1,7,3,9),(5,),(2,4,6,8))
+# Winner combinations
+winners=((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
 
 
 # Corners, Center and Others, respectively
