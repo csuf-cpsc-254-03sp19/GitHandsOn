9d412280706146e7b5e48489660609f264eb9f2a
cspprice <cspprice@gmail.com>
2019-02-28 21:32:11 -0800

    fixed error from copy and pasting, game now works

--- tictactoe.py
+++ tictactoe.py
@@ -10,11 +10,6 @@ moves=((1,7,3,9),(5,),(2,4,6,8))
 # Winner combinations
 winners=((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
 
-# Corners, Center and Others, respectively
-moves=((),(),())
-# Winner combinations
-winners=()
-# Table
 tab=range(1,10)
 
 def print_board():
