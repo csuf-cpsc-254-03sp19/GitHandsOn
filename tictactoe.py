86a3141c069a07401a0e9295a239ec839b09f377
sahilsheth <sahilsheth642@gmail.com>
2019-03-05 11:25:54 -0800

    final patch

--- tictactoe.py
+++ tictactoe.py
@@ -39,16 +39,56 @@ def can_move(brd, player, move):
     return False
 
 def can_win(brd, player, move):
+    places=[]
+    x=0
+    for i in brd:
+      if i == player: places.append(x);
+      x+=1
+    win=True
+    for tup in winners:
+      win=True
+      for ix in tup:
+        if brd[ix]!= player:
+          win=False
+          break
+      if win == True:
+        break
     return win
 
 def make_move(brd, player, move, undo=False):
+    if can_move(brd, player, move):
+      brd[move-1] = player
+      win=can_win(brd, player, move)
+      if undo:
+        brd[move-1] = move-1
+      return (True, win)
+
     return (False, False)
 
 # AI goes here
 def computer_move():
+    move=-1
     # If I can win, others don't matter.
+     for i in range(1,10):
+        if make_move(board, computer, i, True)[1]:
+            move=i
+            break
+    if move == -1:
+
         # If player can win, block him.
+    for i in range(1,10):
+            if make_move(board, player, i, True)[1]:
+                move=i
+                break
+    if move == -1:
+
         # Otherwise, try to take one of desired places.
+         for tup in moves:
+            for mv in tup:
+                if move == -1 and can_move(board, computer, mv):
+                    move=mv
+                    break
+
     return make_move(board, computer, move)
 
 def space_exist():
@@ -60,6 +100,20 @@ print('Player is [%s] and computer is [%s]' % (player, computer))
 result='%%% Deuce ! %%%'
 
 while space_exist():
+     print_board()
+    print('# Make your move ! [1-9] : ', end='')
+    move = int(input())
+    moved, won = make_move(board, player, move)
+    if not moved:
+        print(' >> Invalid number ! Try again !')
+        continue
+    #
+    if won:
+        result='*** Congratulations ! You won ! ***'
+        break
+    elif computer_move()[1]:
+        result='=== You lose ! =='
+
         break;
 
 print_board()
